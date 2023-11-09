4bdbe3bd4141569201a9e047bbfae2242cad8677
Aravind Shajan <117029956+aravindshajan6@users.noreply.github.com>
2023-11-10 01:20:53 +0530

    cors update

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,13 @@ const dbConnection = require('./config/dbConnection');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Allow all origins for development, but be more restrictive in production.
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 const app = express();
 dotenv.config({path: './config/.env'});
 app.use(cors());
