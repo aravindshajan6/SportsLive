08b913c033c2ca3bb57f6fbb64d36ff843427131
Aravind Shajan <aravindshajan6@gmail.com>
2024-08-07 13:35:20 +0530

    update atlas URI

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const dbConnection = () => {
+    console.log("aaaaaaaaaaa  : ", process.env.AAA);
     mongoose.connect(process.env.mongoAtlasURI, 
     {useUnifiedTopology: true},
     {useNewUrlParser: true})
